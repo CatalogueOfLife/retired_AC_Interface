7ecbd692a9e48a71e6a8cc6292eb7e153a1461dd
nuria <naturalis@users.noreply.github.com>
2009-07-30 14:23:18 +0000

    ACI-22 starting from scratch ...

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sql_server = 'localhost';
-$sql_user_name = 'root';
-$sql_password = '';
-$sql_database = 'col2010acv1';
-
-$online_or_offline_version = 'online';
-$show_accepted_names_only = true;
-?>
--- includes/db_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once "config.php";
-$link = mysql_connect($sql_server, $sql_user_name, $sql_password) or die("Error: connection failure: " . mysql_error());
-mysql_select_db($sql_database) or die("Error: could not select database: " . mysql_error());
-?>
