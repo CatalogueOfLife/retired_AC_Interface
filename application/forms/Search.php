4317951e74c10fc5e1d48b74ed6e83bb5fe64c22
Dennis Seijts <naturalis@users.noreply.github.com>
2009-08-18 15:52:36 +0000

    ACI-29 building forms

--- application/forms/Search.php
+++ application/forms/Search.php
@@ -6,12 +6,16 @@ class AC_Form_Search extends Zend_Form
         $this->setAction('');
         $this->setMethod('post');
 
+        $translator = Zend_Registry::get('translator');
+        
         $searchfield = $this->createElement('text','search_string');
         $searchfield->setRequired(true);
-
+        $searchfield->setLabel($translator->translate('Search_for'));
+        
         $match_whole_words = $this->createElement('checkbox','whole_words');
         $match_whole_words->setValue('1');
-        $translator = Zend_Registry::get('translator');
+        $match_whole_words->setLabel($translator->translate('Match_whole_words_only'));
+        
         $submit = $this->createElement('submit',$translator->translate('Search'));
         
         // Add elements to form:
