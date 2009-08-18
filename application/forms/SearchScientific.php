4fc154fd21216c071ce6e81052916b60d89615c1
nuria <naturalis@users.noreply.github.com>
2009-08-18 15:22:22 +0000

    ACI-32

--- application/forms/SearchScientific.php
+++ application/forms/SearchScientific.php
@@ -6,9 +6,9 @@ class AC_FormScientific_Search extends Zend_Form
         $this->setAction('');
         $this->setMethod('post');
 
-        $searchfield = $this->createElement('text','search_genus');
-        $searchfield = $this->createElement('text','search_speices');
-        $searchfield = $this->createElement('text','search_infraspecies');
+        $genus        = $this->createElement('text','search_genus');
+        $species      = $this->createElement('text','search_species');
+        $infraspecies = $this->createElement('text','search_infraspecies');
         
         $match_whole_words = $this->createElement('checkbox','whole_words');
         $match_whole_words->setValue('1');
@@ -18,7 +18,9 @@ class AC_FormScientific_Search extends Zend_Form
         $submit = $this->createElement('submit',$translator->translate('Search'));
         
         // Add elements to form:
-        $this->addElement($searchfield)
+        $this->addElement($genus)
+             ->addElement($species)
+             ->addElement($infraspecies)
              ->addElement($match_whole_words)
              ->addElement($clear)
              ->addElement($submit);
