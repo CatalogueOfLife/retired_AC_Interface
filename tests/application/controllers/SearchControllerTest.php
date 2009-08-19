a9e3feeb1c76dc33f52f6fad685662bb98f60e9e
nuria <naturalis@users.noreply.github.com>
2009-08-19 15:49:57 +0000

    ACI-35 fixing test setup

--- tests/application/controllers/SearchControllerTest.php
+++ tests/application/controllers/SearchControllerTest.php
@@ -1,23 +1,22 @@
 <?php
-require_once 'PHPUnit/Framework/TestCase.php';
+require_once 'Zend/Test/PHPUnit/ControllerTestCase.php';
 
 class SearchControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 {
     public function setUp ()
     {
         parent::setUp();
-        $this->frontController->setControllerDirectory(
-            array(
-                'default' => APPLICATION_PATH . '/modules/default/controllers')
-            );
-        $this->frontController->setDefaultModule('default');
+        $this->frontController->addControllerDirectory(
+            APPLICATION_PATH . '/controllers');
     }
+    
     public function testHomePageIsASuccessfulRequestToSearchAll ()
     {
         // Runs the test on /, the homepage
         $this->dispatch('/');
         // Tests there are no exceptions on the home page
         $this->assertFalse($this->response->isException());
+        $this->assertResponseCode(200);
         // Tests for redirection to the error handler
         $this->assertNotRedirect();
         $this->assertController('search');
