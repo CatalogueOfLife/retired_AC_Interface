596e6432936d49efb838ee71fe0d38fa8e1ae047
nuria <naturalis@users.noreply.github.com>
2009-08-20 10:31:01 +0000

    ACI-32 redirecting flow errors to the index page

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -4,27 +4,27 @@ class ErrorController extends Zend_Controller_Action
 {
     public function errorAction()
     {
-        $this->view->layout()->disableLayout();
-        
         $errors = $this->_getParam('error_handler');
         
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-        
-                // 404 error -- controller or action not found
-                $this->getResponse()->setHttpResponseCode(404);
-                $this->view->message = 'Page not found';
+                // On controller/action not found, redirect to the default page
+                $front = Zend_Controller_Front::getInstance();
+                $this->_redirect(
+                    $front->getDefaultControllerName() . '/' .
+                    $front->getDefaultAction()
+                );
                 break;
             default:
                 // application error
+                $this->view->layout()->disableLayout();
                 $this->getResponse()->setHttpResponseCode(500);
                 $this->view->message = 'Application error';
+                $this->view->exception = $errors->exception;
+                $this->view->request   = $errors->request;
                 break;
         }
-        
-        $this->view->exception = $errors->exception;
-        $this->view->request   = $errors->request;
     }
 
 
