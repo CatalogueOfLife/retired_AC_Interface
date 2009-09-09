775ca162cd1157ef78196c4cbd4fe98a29a12de8
nuria <naturalis@users.noreply.github.com>
2009-09-09 09:40:40 +0000

    ACI-58 fixing menu for IE6

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,19 +1,53 @@
-// JavaScript Document
-
 startList = function() {
-if (document.all&&document.getElementById) {
-navRoot = document.getElementById("nav");
-for (i=0; i<navRoot.childNodes.length; i++) {
-node = navRoot.childNodes[i];
-if (node.nodeName=="LI") {
-node.onmouseover=function() {
-this.className+=" over";
-  }
-  node.onmouseout=function() {
-  this.className=this.className.replace(" over", "");
-   }
-   }
-  }
- }
+    if (document.all && document.getElementById) {
+        navRoot = document.getElementById("nav");
+        for (i = 0; i < navRoot.childNodes.length; i++) {
+            node = navRoot.childNodes[i];
+            if (node.nodeName == "LI") {
+                node.onmouseover = function() {
+                    this.className += " over";
+                }
+                node.onmouseout = function() {
+                    this.className = this.className.replace(" over", "");
+                }
+                ulNodes = node.getElementsByTagName("UL");
+                for (j = 0; j < ulNodes[0].childNodes.length; j++) {
+                    ulLiNode = ulNodes[0].childNodes[j];                
+                    if (ulLiNode.nodeName == "LI") {
+                        ulLiNode.onmouseover = function() {
+                            this.className += " over";
+                        }
+                        ulLiNode.onmouseout = function() {
+                            this.className = this.className.replace(" over", "");
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-window.onload=startList;
+moveMenu = function() {
+    var menuLayer = document.getElementById('menu');    
+    var theScroll = 0;
+    if (window.pageYOffset) {
+        theScroll = window.pageYOffset;        
+    } else if (window.document.documentElement            
+            && window.document.documentElement.scrollTop) {
+        //IE6
+        theScroll = window.document.documentElement.scrollTop;        
+    } else if (window.document.body) {
+        //Firefox
+        theScroll = window.document.body.scrollTop;        
+    }
+    var newY = theScroll + "px";
+    if (menuLayer) {
+        menuLayer.style.top = newY;
+        setTimeout("moveMenu()", 500);
+    }
+}
+init = function() {
+    startList();
+    moveMenu();
+}
+window.onload = init;
+window.onscroll = moveMenu;
