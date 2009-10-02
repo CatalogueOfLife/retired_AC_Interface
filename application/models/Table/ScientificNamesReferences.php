1810c252d4e5b9bb9624e1248316d9643babfe3d
nuria <naturalis@users.noreply.github.com>
2009-10-02 09:38:29 +0000

    ACI-44

---
+++ application/models/Table/ScientificNamesReferences.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Annual Checklist Interface
+ *
+ * Class ACI_Model_Table_ScientificNameReferences
+ * Scientific names - references relationship table model
+ *
+ * @category    ACI
+ * @package     application
+ * @subpackage  models
+ *
+ */
+class ACI_Model_Table_ScientificNameReferences extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'scientific_name_references';
+    protected $_primary = 'record_id';
+    
+    public function get ($nameCode)
+    {
+        $select = $this->select(true)->where('name_code = ? AND reference_type <> "ComNameRef"', $nameCode);
+        $stmt = $this->_db->query($select);
+        $data = $stmt->fetchAll();
+        return $this->_filterData($data);
+    }
+    
+    protected function _filterData($data) {
+        $filteredData = array();
+        foreach($data as $d) {
+            $filteredData[] = $d['reference_id'];
+        }
+        return $filteredData;
+    }
+}
