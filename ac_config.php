2451ed2f7641f0ae1ed44b537e646bd82f836b7d
nuria <naturalis@users.noreply.github.com>
2009-11-06 14:43:11 +0000

    ACI-115 Resetting credentials

--- ac_config.php
+++ ac_config.php
@@ -2,8 +2,8 @@
 	$online_or_offline_version = "offline";
 	// SQL settings
 		$sql_server = "localhost" ;
-		$sql_user_name = "webuser" ;
-		$sql_password = "codatanl" ;
+		$sql_user_name = "user" ;
+		$sql_password = "password" ;
 		$sql_database = "col2008ac" ;
 
 ?>
