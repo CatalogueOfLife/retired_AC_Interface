504e193ff3484feea6b611d59c0ecba8a72f6186
nuria <naturalis@users.noreply.github.com>
2009-11-06 14:43:11 +0000

    ACI-115 Resetting credentials

--- connect_to_database.php
+++ connect_to_database.php
@@ -1,4 +1,4 @@
 <?php
-	$link = mysql_connect("localhost", "webuser", "codatanl") or die("Error: connection failure: " . mysql_error());
+	$link = mysql_connect("localhost", "user", "password") or die("Error: connection failure: " . mysql_error());
 	mysql_select_db("col2005ac") or die("Error: could not select database");
 ?>
