d2072ad57f6eaf4f99949446d812e70cde8de86b
nuria <naturalis@users.noreply.github.com>
2009-11-13 15:18:59 +0000

    ACI-132

--- application/forms/Dojo/InfoNavigator.php
+++ application/forms/Dojo/InfoNavigator.php
@@ -15,11 +15,12 @@ class ACI_Form_Dojo_InfoNavigator extends Zend_Dojo_Form
     public function init()
     {
         $this->setMethod(Zend_Form::METHOD_GET);
+        $translator = Zend_Registry::get('Zend_Translate');
         $pages = $this->createElement('Select', 'page');
-        $next = $this->createElement('Button', 'next', array('onclick', 'javascript:alert("hoi")'))
-            ->setLabel('Next');
+        $next = $this->createElement('Button', 'next')
+            ->setLabel($translator->translate('Next') . ' >>');
         $previous = $this->createElement('Button', 'previous')
-            ->setLabel('Previous');
+            ->setLabel('<< ' . $translator->translate('Previous'));
         $this->addElement($previous)->addElement($pages)->addElement($next);
     }
 }
