ae011aad2113490b38634c96c5caa28f75259610
nuria <naturalis@users.noreply.github.com>
2009-11-24 13:24:27 +0000

    ACI-138

--- tests/application/controllers/DetailsControllerTest.php
+++ tests/application/controllers/DetailsControllerTest.php
@@ -45,7 +45,7 @@ class DetailsControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
     }
     
     /**
-     * The species details page correctly retrieves and displays the species 
+     * The species details page correctly retrieves and displays the species
      * from the given id
      */
     public function testSpeciesDetailsDisplay()
@@ -60,7 +60,7 @@ class DetailsControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->assertQueryCount('table.details-table', 1);
         //Scientific name
         $this->assertQueryContentContains(
-            'table.details-table tr td', 
+            'table.details-table tr td',
             '<i>Aa rosei</i> Ames (accepted name)'
         );
         //Source database
@@ -68,20 +68,20 @@ class DetailsControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
             'table.details-table tr td a',
             'World Checklist of Selected Plant Families'
         );
-        //LSID 
+        //LSID
         $this->assertQueryContentContains(
-            'table.details-table tr td p.lsid', 
-            'urn:lsid:catalogueoflife.org:taxon:f0c67b16-29c1-102b-9a4a-' . 
+            'table.details-table tr td span.lsid',
+            'urn:lsid:catalogueoflife.org:taxon:f0c67b16-29c1-102b-9a4a-' .
             '00304854f820:ac2009'
-        );   
+        );
     }
     
     /**
-     * The database details page correctly retrieves and displays the database 
+     * The database details page correctly retrieves and displays the database
      * from the given id
      */
     public function testDatabaseDetailsDisplay()
-    {        
+    {
         $id = 10;
         $database = new ACI_Model_Table_Databases();
         $database->get($id);
@@ -91,12 +91,12 @@ class DetailsControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         //Details table
         $this->assertQueryCount('table.details-table', 1);
         //Logo
-        $this->assertQueryCount('a img.wrapLogo', 1);     
+        $this->assertQueryCount('div.wrapLogo a img', 1);
         //Name
         $this->assertQueryContentContains('table tr td', 'FishBase');
         //Link
         $this->assertQueryContentContains(
             'table.details-table tr td', 'http://www.fishbase.org'
-        );    
+        );
     }
 }
