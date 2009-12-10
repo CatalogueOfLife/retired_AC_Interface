e2f7340f7c4545b8e3a97edccd639d452636041a
nuria <naturalis@users.noreply.github.com>
2009-12-10 12:36:06 +0000

    ACI-181 back to 0.5s

--- application/forms/Dojo/AMultiCombo.php
+++ application/forms/Dojo/AMultiCombo.php
@@ -39,7 +39,7 @@ abstract class ACI_Form_Dojo_AMultiCombo extends Zend_Dojo_Form
                         'highlightMatch' => 'none',
                         'queryExpr' => '*${0}*',
                         'searchAttr' => 'name',
-                        'searchDelay' => 1000,
+                        'searchDelay' => 500,
                         'onChange' => 'updateKey'
                     ),
                     'style' => 'width: 300px'
