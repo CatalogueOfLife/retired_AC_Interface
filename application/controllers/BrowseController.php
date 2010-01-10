cf02766b26a86cbc8e85497a1c10c1acf4cd5e57
nuria <naturalis@users.noreply.github.com>
2010-01-10 17:15:22 +0000

    ACI-195

--- application/controllers/BrowseController.php
+++ application/controllers/BrowseController.php
@@ -13,6 +13,9 @@ require_once 'AController.php';
  */
 class BrowseController extends AController
 {
+    // Tree persistance
+    protected $_persistTree = false;
+    
     public function treeAction()
     {
         $fetch = $this->_getParam('fetch', false);
@@ -28,16 +31,7 @@ class BrowseController extends AController
         else {
             $id = $this->_getParam('id', false);
         }
-        // Tree persistance (disabled)
-        // If no id or species was passed
-        /*if(!$id) {
-            // get the id from persistance (session)
-            $id = $this->getHelper('SessionHandler')->get('tree_id', false);
-        }
-        else {
-            // persist current id in session
-            $this->getHelper('SessionHandler')->set('tree_id', $id, false);
-        }*/
+        $this->_persistTree($id);
         
         $hierarchy = array();
         if ($id !== false) {
@@ -56,6 +50,30 @@ class BrowseController extends AController
              ->requireModule('ACI.dojo.TxStoreModel')
              ->requireModule('ACI.dojo.TxTree')
              ->requireModule('ACI.dojo.TxTreeNode');
+    }
+    
+    /**
+     * Stores/retrieves the latest status of the tree if tree presistance is
+     * enabled
+     *
+     * @param int $id
+     * @return boolean
+     */
+    protected function _persistTree(&$id)
+    {
+        if(!$this->_persistTree) {
+            return false;
+        }
+        // If no id or species was passed
+        if(!$id) {
+            // get the id from persistance (session)
+            $id = $this->getHelper('SessionHandler')->get('tree_id', false);
+        }
+        else {
+            // persist current id in session
+            $this->getHelper('SessionHandler')->set('tree_id', $id, false);
+        }
+        return true;
     }
     
     public function classificationAction()
--- tests/application/controllers/BrowseControllerTest.php
+++ tests/application/controllers/BrowseControllerTest.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'mocks/BrowseControllerMock.php';
 /**
  * Annual Checklist Interface
  *
@@ -16,7 +17,7 @@ class BrowseControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
     {
         parent::setUp();
         $this->frontController
-             ->addControllerDirectory(APPLICATION_PATH . '/controllers');
+             ->addControllerDirectory(APPLICATION_PATH . '/controllers');        
     }
     /**
      * Browse controller redirects requests with no action to browse/tree
@@ -50,5 +51,70 @@ class BrowseControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->dispatch('/browse/classification');
         $this->assertController('browse');
         $this->assertAction('classification');
+    }
+    
+    public function testTreePersistanceEnabled()
+    {
+        // Set session
+        $storedId = 300;
+        $sess = $this->_getSession();
+        $sess->set('tree_id', $storedId, false);
+        $bc = $this->_getBrowseController();
+        $bc->setTreePersistance(true);
+        $id = null;
+        $bc->persistTree($id);
+        // $id has to be reset to the stored value
+        $this->assertEquals($storedId, $id);
+    }
+    
+    public function testTreePersistanceStored()
+    {
+        // Set session
+        $storedId = 400;
+        $sess = $this->_getSession();
+        $sess->set('tree_id', $storedId, false);
+        $bc = $this->_getBrowseController();
+        $bc->setTreePersistance(true);
+        $id = 200;
+        $bc->persistTree($id);
+        // $id remains the same
+        $this->assertEquals($id, 200);
+        // and it has been stored in session
+        $this->assertEquals($sess->get('tree_id', false), $id);
+    }
+    
+    public function testTreePersistanceDisabled()
+    {
+        // Clear session
+        $sess = $this->_getSession();
+        $bc = $this->_getBrowseController();
+        $bc->setTreePersistance(false);
+        $id = 100;
+        $bc->persistTree($id);
+        $this->assertEquals($id, 100);
+        // No tree id set in session (null)
+        $this->assertNull($sess->get('tree_id', false));
+    }
+    
+    protected function _getSession()
+    {
+        $sess = new ACI_Helper_SessionHandler();        
+        $sess->init();
+        return $sess;
+    }
+    
+    protected function _getBrowseController()
+    {
+        $bc = new BrowseControllerMock(
+            $this->getRequest(), $this->getResponse()
+        );
+        return $bc;
+    }
+    
+    public function tearDown()
+    {
+        // Clear session
+        $sess = $this->_getSession();
+        $sess->clear();
     }
 }
