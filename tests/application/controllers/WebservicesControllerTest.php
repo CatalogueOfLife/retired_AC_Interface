ee1d676f2b4368a77782858ffaa840c34ae730a7
nuria <naturalis@users.noreply.github.com>
2010-02-11 22:09:12 +0000

    ACI-211

--- tests/application/controllers/WebservicesControllerTest.php
+++ tests/application/controllers/WebservicesControllerTest.php
@@ -40,10 +40,31 @@ class WebservicesControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->dispatch('/webservices');
         $this->assertXpathCount('//results', 1);
         $this->assertXpath("//results[@id = '']");
+        $this->assertXpath("//results[@name = '']");
         $this->assertXpath("//results[@total_number_of_results = '0']");
         $this->assertXpath("//results[@start = '0']");
         $this->assertXpath("//results[@number_of_results_returned = '0']");
         $this->assertXpath("//results[@error_message = 'No name or ID given']");
         $this->assertXpath("//results[@version = '1.0']");
+    }
+    
+    public function testEmptySerializedResponse()
+    {   
+        $expectedResponse = array(
+            'id' => '',
+            'name' => '',
+            'total_number_of_results' => 0,
+            'start' => 0,
+            'number_of_results_returned' => 0,
+            'error_message' => 'No name or ID given',
+            'version' => '1.0'
+        );
+        $this->dispatch('/webservices/?format=php');
+        $this->assertEquals(
+            array_diff_assoc(
+                unserialize($this->getResponse()->getBody()),
+                $expectedResponse
+            ), array()
+        );
     }
 }
