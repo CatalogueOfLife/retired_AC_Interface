41922f4665ccc2170816a2c41ed4efd416ea30b8
nuria <naturalis@users.noreply.github.com>
2010-03-04 12:51:17 +0000

    ACI-219

--- application/models/Webservice.php
+++ application/models/Webservice.php
@@ -28,8 +28,8 @@ class ACI_Model_Webservice extends AModel
         'total_number_of_results' => 0,
         'number_of_results_returned' => 0,
         'start' => 0,
-        'error_message' => self::VERSION,
-        'version' => ''
+        'error_message' => '',
+        'version' => self::VERSION
     );
     protected $_model;
     protected $_detailsModel;
