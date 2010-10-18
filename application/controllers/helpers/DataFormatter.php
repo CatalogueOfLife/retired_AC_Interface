d52f118d6c0630e742903ae67e18ac8e5f2a6bdd
Dennis Seijts <naturalis@users.noreply.github.com>
2010-10-18 09:51:20 +0000

    ACI-278

--- application/controllers/helpers/DataFormatter.php
+++ application/controllers/helpers/DataFormatter.php
@@ -100,7 +100,7 @@ class ACI_Helper_DataFormatter extends Zend_Controller_Action_Helper_Abstract
                 ),
                 $row['status'],
                 $row['status'] == 6 ?
-                $row['language'] : $row['author']
+                $row['name_suffix'] : $row['author']
             );
             $res[$i]['rank'] = $translator->translate(
                 ACI_Model_Table_Taxa::getRankString($row['rank'])
