772cd770c9b024f4c3aa292efd3881b5f1f5ad6f
Dennis Seijts <naturalis@users.noreply.github.com>
2011-03-10 14:33:25 +0000

    ACI-372 Added langauge div (now all languages set cookies)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -162,3 +162,14 @@ function sendAjaxRequestForCookie(className, value) {
 	xmlhttp.open("GET",baseUrl + "/browse/tree-update-cookie/" + className + "/" + value,true);
 	xmlhttp.send();
 }
+
+function setCookie(c_name,value,exdays) {
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value + ";path=/";
+}
+
+function changeLanguage(language) {
+	setCookie('language',language,14);
+}
