fe0686e9f5c557c997ebd951589d20b89c831506
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2011-03-10 13:32:11 +0000

    ACI-372 Chinese and Spanish traanslation files

--- CreateDummyDataScripts/arkive.php
+++ CreateDummyDataScripts/arkive.php
@@ -19,7 +19,7 @@
 	$added = 0;
 	
     //$query = 'SELECT `taxon_id`, `genus_name`, `species_name`, `infraspecies_name` FROM `_species_details`';
-    $query = 'SELECT `taxon_id`, `genus_name`, `species_name`, `infraspecies_name` FROM `_species_details` WHERE `taxon_id` > 6964602';
+    $query = 'SELECT `taxon_id`, `genus_name`, `species_name`, `infraspecies_name` FROM `_species_details` WHERE `taxon_id` > 6992572';
     $result = mysql_query($query) or die(mysql_error());
 	$total = mysql_num_rows($result) or die(mysql_error());
 	while ($row = mysql_fetch_array($result)) {
