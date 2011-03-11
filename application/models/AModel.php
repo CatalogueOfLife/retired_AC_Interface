6186374ffa83be8615e98a3b53833cac4db39720
Dennis Seijts <naturalis@users.noreply.github.com>
2011-03-11 13:03:38 +0000

    ACI-372 Added the Amodel..., because it's very usefull and it prefents the search function otherwise...

---
+++ application/models/AModel.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Annual Checklist Interface
+ *
+ * Class AModel
+ * Abstract model class
+ *
+ * @category    ACI
+ * @package     application
+ * @subpackage  models
+ *
+ */
+abstract class AModel
+{
+    protected $_db;
+    protected $_logger;
+    
+    public function __construct(Zend_Db_Adapter_Abstract $dbAdapter)
+    {
+        $this->_db = $dbAdapter;
+        $this->_logger = Zend_Registry::get('logger');
+    }
+    
+    public function getFoundRows()
+    {        
+        $select = new Eti_Db_Select($this->_db);
+        $select->from(null, new Zend_Db_Expr('FOUND_ROWS()'));
+        return $select->query()->fetchColumn(0);
+    }
+    
+    protected function _moduleEnabled($module) {
+        return Bootstrap::instance()->getOption('module.'.$module);
+    }
+}
